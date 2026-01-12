9d66adb19686b0d1a73457364929f9d84dd00d95
DoctorWho907 <doctorwhotardiseku@gmail.com>
2026-01-12 13:05:41 -0500

    Add comments for pin configuration clarity

--- Code/sketches/traffic.ino
+++ Code/sketches/traffic.ino
@@ -5,7 +5,7 @@ const int lightOnePinThree = 8;
 const int lightTwoPinOne = 13;
 const int lightTwoPinTwo = 12;
 const int lightTwoPinThree = 8;
-// done till here
+// one set of pins
 const int lightThreePinOne = 7; 
 const int lightThreePinTwo = 6;
 const int lightThreePinThree = 5;
