5c08f628766ad3d825fbed778c6f2569768c6fba
DoctorWho907 <doctorwhotardiseku@gmail.com>
2026-01-12 13:06:37 -0500

    Update comment for light set in traffic.ino

--- Code/sketches/traffic.ino
+++ Code/sketches/traffic.ino
@@ -5,7 +5,7 @@ const int lightOnePinThree = 8;
 const int lightTwoPinOne = 13;
 const int lightTwoPinTwo = 12;
 const int lightTwoPinThree = 8;
-// one set of lights
+// one set of lights -- Above
 const int lightThreePinOne = 7; 
 const int lightThreePinTwo = 6;
 const int lightThreePinThree = 5;
