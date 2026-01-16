304ed800060b78538e928e371f4f1fbd65c8fae5
doctorwho907 <doctorwhotardiseku@gmail.com>
2026-01-16 02:15:41 -0500

    Fixed Traffic Light Logic --- IDK HOW IT WAS WORKING IT SHOULDNT HAVE Added Crosswalk Lights Logic Optimised Code Formatted Code

--- Code/traffic.ino
+++ Code/traffic.ino
@@ -1,12 +1,24 @@
 // INITIALIZE VARIBLES
-const int lightSet1Green = 13;
+//Traffic Lights
+//Set 1 of lights
+const int lightSet1Red = 13;
 const int lightSet1Yellow = 12; 
-const int lightSet1Red = 8;
-// one set of lights -- Above
-const int lightSet2Green = 7; 
+const int lightSet1Green = 8;
+//Set 2 of traffic lights
+const int lightSet2Red = 7; 
 const int lightSet2Yellow = 4;
-const int lightSet2Red = 2;
-//set 2 of traffic lights -- Above
+const int lightSet2Green = 2;
+
+//Pedestrian Crossings
+//Set 1 of crossings
+const int crossingSet1Green = 5;
+const int crossingSet1Blue = 6;
+const int crossingSet1Red = 3;
+//Set 2 of crossings
+const int crossingSet2Green = 10;
+const int crossingSet2Blue = 11;
+const int crossingSet2Red = 9;
+//Misc
 int photoresistorVal = 0;
 
 void setup() {
@@ -32,6 +44,7 @@ void setup() {
   digitalWrite(lightSet2Green, LOW);
   digitalWrite(lightSet2Yellow, LOW);
   digitalWrite(lightSet2Red, LOW);
+  
   //setup photoresistor
   Serial.begin(9600);
   digitalWrite(A0, HIGH);
@@ -39,80 +52,52 @@ void setup() {
 
 void loop() {
 // Traffic Light Cycle -------------------------------------------------------
-read_photoresistor();
 street_lights();
+red_one();
+red_two();
 delay(100);
-read_photoresistor();
 street_lights();
-// Turn opposite lights RED to start the cycle
-digitalWrite(lightSet2Red, HIGH);
-// Start cycle - Green lights
-digitalWrite(lightSet1Green, HIGH);
-delay(5000);
-read_photoresistor();
-street_lights();
-// Turn Green Lights Off
-digitalWrite(lightSet2Red, LOW);
 
-// Turn Lights Yellow
-digitalWrite(lightSet2Yellow, HIGH);
-delay(5000);
-read_photoresistor();
-street_lights();
-//Turn Yellow Lights Off
-digitalWrite(lightSet2Yellow, LOW);
-
-//Turn Red Lights On
-digitalWrite(lightSet2Green, HIGH);
- 
-// Turn lights yellow
-digitalWrite(lightSet1Green, LOW);
-
-digitalWrite(lightSet1Yellow, HIGH);
+//set 2 red and set 1 Yellow
+yr();
+yellow_one();
+red_two();
 delay(2500);
-read_photoresistor();
+
+//set 1 green
 street_lights();
-// Red light
-digitalWrite(lightSet1Yellow, LOW);
-
-digitalWrite(lightSet1Red, HIGH);
-
-delay(1500);
-read_photoresistor();
-street_lights();
-// Start cycle - Green lights (Opposite Lights)
-digitalWrite(lightSet2Green, HIGH);
-
+gr();
+white_one();
 delay(5000);
-read_photoresistor();
 street_lights();
-// Turn lights yellow
-digitalWrite(lightSet2Green, HIGH);
-digitalWrite(lightSet1Yellow, HIGH);
-digitalWrite(lightSet1Red, LOW);
-delay(1500);
-read_photoresistor();
-street_lights();
-digitalWrite(lightSet1Yellow, LOW);
-digitalWrite(lightSet1Red, LOW);
-digitalWrite(lightSet2Yellow, HIGH);
 
-digitalWrite(lightSet1Green, HIGH);
-digitalWrite(lightSet2Green, LOW);
-delay(2500);
-read_photoresistor();
+// set 1 yelllow
+yr();
+flashing_one();
 street_lights();
-// Red light
-digitalWrite(lightSet2Yellow, LOW);
-  
-digitalWrite(lightSet2Green, LOW);
+
+//set 1 red and set 2 yellow
+ry();
+red_one();
+yellow_two();
+delay(2500);
+street_lights();
+
+// set 2 green
+rg();
+white_two();
+delay(5000);
+street_lights();
+
+// set 2 yellow
+ry();
+flashing_two();
+street_lights();
 }
 
-void read_photoresistor(){
+void street_lights(){
 photoresistorVal = analogRead(A0);
 Serial.print(photoresistorVal);
-}
-void street_lights(){
 if (photoresistorVal>500){
 digitalWrite(A1, HIGH);
 }
@@ -120,40 +105,130 @@ else{
 digitalWrite(A1,LOW);
 }
 }
-// TODO - EKANSH: CHANGE ALL FUNCTION VARIABLES TO THE PINS NEEDED FOR THE RGB LEDS
-/*
-void green () {
+
+//Colours for set 1
+void green_one () {
   //set the LED pins to values that make green
-  analogWrite(RedPin, 0);
-  analogWrite(GreenPin, 100);
-  analogWrite(BluePin, 0);
+  analogWrite(crossingSet1Red, 0);
+  analogWrite(crossingSet1Green, 100);
+  analogWrite(crossingSet1Blue, 0);
 }
 
-void yellow () {
+void yellow_one () {
   //set the LED pins to values that make yellow
-  analogWrite(RedPin, 100);
-  analogWrite(GreenPin, 100);
-  analogWrite(BluePin, 0);
+  analogWrite(crossingSet1Red, 100);
+  analogWrite(crossingSet1Green, 100);
+  analogWrite(crossingSet1Blue, 0);
 }
 
-void red () {
+void red_one () {
   //set the LED pins to values that make red
-  analogWrite(RedPin, 100);
-  analogWrite(GreenPin, 0);
-  analogWrite(BluePin, 0);
+    analogWrite(crossingSet1Red, 100);
+  analogWrite(crossingSet1Green, 0);
+  analogWrite(crossingSet1Blue, 0);
 }
 
-void white () {
+void white_one () {
   //set the LED pins to values that make white
-  analogWrite(RedPin, 255);
-  analogWrite(GreenPin, 255);
-  analogWrite(BluePin, 255);
+  analogWrite(crossingSet1Red, 255);
+  analogWrite(crossingSet1Green, 255);
+  analogWrite(crossingSet1Blue, 255);
 }
 
-void off () {
-  analogWrite(RedPin, 0);
-  analogWrite(GreenPin, 0);
-  analogWrite(BluePin, 0);
-} 
-*/
+void flashing_one() {
+  float start_time = millis();
+  const float duration = 3500;
+  while (millis() - start_time <= duration) {
+  red_one();
+  delay(200);
+  off_one();
+  delay(200);
+  }
+}
 
+void off_one () {
+  analogWrite(crossingSet1Red, 0);
+  analogWrite(crossingSet1Green, 0);
+  analogWrite(crossingSet1Blue, 0);
+}
+//Colours for set 2
+void green_two () {
+  //set the LED pins to values that make green
+  analogWrite(crossingSet2Red, 0);
+  analogWrite(crossingSet2Green, 100);
+  analogWrite(crossingSet2Blue, 0);
+}
+
+void yellow_two () {
+  //set the LED pins to values that make yellow
+  analogWrite(crossingSet2Red, 100);
+  analogWrite(crossingSet2Green, 100);
+  analogWrite(crossingSet2Blue, 0);
+}
+
+void red_two () {
+  //set the LED pins to values that make red
+  analogWrite(crossingSet2Red, 100);
+  analogWrite(crossingSet2Green, 0);
+  analogWrite(crossingSet2Blue, 0);
+}
+
+void flashing_two() {
+  float start_time = millis();
+  const float duration = 3500;
+  while (millis() - start_time <= duration) {
+    red_two();
+    delay(200);
+    off_two();
+    delay(200);
+  }
+}
+void white_two () {
+  //set the LED pins to values that make white
+  analogWrite(crossingSet2Red, 255);
+  analogWrite(crossingSet2Green, 255);
+  analogWrite(crossingSet2Blue, 255);
+}
+
+void off_two() {
+  analogWrite(crossingSet2Red, 0);
+  analogWrite(crossingSet2Green, 0);
+  analogWrite(crossingSet2Blue, 0);
+}
+
+//lights
+void yr(){
+digitalWrite(lightSet2Yellow, LOW);
+digitalWrite(lightSet2Green, LOW);
+digitalWrite(lightSet1Red, LOW);
+digitalWrite(lightSet2Red, HIGH);
+digitalWrite(lightSet1Yellow, HIGH);
+digitalWrite(lightSet1Green, LOW);
+}
+
+void gr(){
+digitalWrite(lightSet2Yellow, LOW);
+digitalWrite(lightSet2Green, LOW);
+digitalWrite(lightSet1Red, LOW);
+digitalWrite(lightSet2Red, HIGH);
+digitalWrite(lightSet1Yellow, LOW);
+digitalWrite(lightSet1Green, HIGH);
+}
+
+void ry(){
+digitalWrite(lightSet2Yellow, HIGH);
+digitalWrite(lightSet2Green, LOW);
+digitalWrite(lightSet1Red, HIGH);
+digitalWrite(lightSet2Red, LOW);
+digitalWrite(lightSet1Yellow, LOW);
+digitalWrite(lightSet1Green, LOW);
+}
+
+void rg(){
+digitalWrite(lightSet2Yellow, LOW);
+digitalWrite(lightSet2Green, HIGH);
+digitalWrite(lightSet1Red, HIGH);
+digitalWrite(lightSet2Red, LOW);
+digitalWrite(lightSet1Yellow, LOW);
+digitalWrite(lightSet1Green, LOW);
+}
